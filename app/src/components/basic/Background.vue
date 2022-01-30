dbfcf29865b7ad16bca206db5925fdcd62e95641
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-01-30 18:32:16 +0300

    Исправлен внешний вид фона

--- app/src/components/basic/Background.vue
+++ app/src/components/basic/Background.vue
@@ -1,8 +1,10 @@
 <template>
 
   <div class="screen screen__background" :style=filter>
-    <div class="svg-wrapper" :style=imageUrl></div>
-    <div class="screen bg-under"></div>
+    <div class="svg-wrapper" :style=bevelUrl></div>
+    <div v-if="stripeEffect" class="screen screen__effects-stripe"></div>
+    <div class="screen bg-layer_animated"></div>
+    <div class="screen bg-layer_textured" :style=backgroundUrl></div>
 
     <audio ref="bgAudio" loop hidden>
       <source :src=audioUrl type="audio/ogg">
@@ -15,51 +17,57 @@
 <script type="ts">
 import { defineComponent } from 'vue'
 import bevelSvg from '@/assets/img/bevel.svg'
+import background from '@/assets/img/bg.png'
 import bgAudio from '@/assets/sound/bg.ogg'
 
 export default defineComponent({
 
-  // hue, opacity, brightness
-  // 190, 85, 120 - red
-  // 315, 75, 200 - green
-  // 140, 80, 80 - cyberpink
+  data: () => ({
+    themes: {
+      // hue, opacity, brightness
+      'default': [149, 80, 80],
+      'red': [190, 85, 120],
+      'green': [315, 75, 200]
+    }
+  }),
 
   props: {
-    hue: {
-      type: Number,
-      default: 0
-    },
-    brightness: {
-      type: Number,
-      default: 100
-    },
-    opacity: {
-      type: Number,
-      default: 100
+    theme: {
+      type: String,
+      default: 'green'
     },
     muted: {
+      type: Boolean,
+      default: false
+    },
+    stripeEffect: {
       type: Boolean,
       default: false
     }
   },
 
   computed: {
-    imageUrl() {
-      return {backgroundImage: `url(${bevelSvg})`};
+    bevelUrl() {
+      return {backgroundImage: `url(${bevelSvg})`}
+    },
+    backgroundUrl() {
+      return {backgroundImage: `url(${background})`}
     },
     audioUrl() {
       return bgAudio
     },
     filter() {
+      const [hue, opacity, brightness] = [...this.themes[this.theme]]
       return {
-        filter: `hue-rotate(${this.hue}deg) brightness(${this.brightness}%)`,
-        opacity: `${this.opacity}%`,
+        filter: `hue-rotate(${hue}deg) brightness(${brightness}%)`,
+        opacity: `${opacity}%`,
       };
     }
   },
 
   mounted() {
     if (!this.muted) this.$refs.bgAudio.play()
+    [this.hue, this.opacity, this.brightness] = [...this.themes[this.theme]]
   }
 
 })
@@ -78,7 +86,7 @@ export default defineComponent({
   z-index: -100;
 }
 
-.bg-under {
+.bg-layer_animated {
   opacity: 0.2;
   background-image:
       radial-gradient(
@@ -86,16 +94,22 @@ export default defineComponent({
               gray,
               black 90%
       );
-  animation: pulse 15s linear infinite;
+  animation: pulse 5s infinite alternate;
+}
+
+.bg-layer_textured {
+  opacity: 0.1;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
 
 @keyframes pulse {
-  0% { opacity: .25; }
-  25% { opacity: .18; }
+  0% { opacity: .15; }
+  25% { opacity: .1; }
   50% { opacity: .2; }
-  75% { opacity: .18; }
-  100% { opacity: .25; }
+  75% { opacity: .1; }
+  100% { opacity: .15; }
 }
 
 </style>
