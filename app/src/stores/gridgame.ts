481678f8f91c6208a3d5f072a407ded79058d1ea
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-06 14:30:08 +0400

    Pinia store state

---
+++ app/src/stores/gridgame.ts
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia';
+import { RESULTS } from '~/config/constants.ts'
+import { generateField } from '@/services/gridgame';
+import type { CellData, GridGameState } from '@/typings/modules/gridgame.ts';
+
+const [minVal, maxVal] = [5, 7];
+const rangeErrorMessage = `Value should be in range from ${minVal} to ${maxVal}`;
+
+export const useStore = defineStore('gridgame', {
+  state: (): GridGameState => ({
+    tries: 0,
+    step: 0,
+    size: 0,
+    field: [],
+    selected: [],
+    result: RESULTS.NONE
+  }),
+
+  actions: {
+    init() {
+      if (!this.size) throw new Error(`You should use setSize(size) method first. ${rangeErrorMessage}`)
+      this.selected = []
+      this.tries = 0
+      this.field = generateField(this.size);
+    },
+
+    setField(value: CellData[]) {
+      this.field = value;
+    },
+
+    setSize(value: number) {
+      if (value < minVal || value > maxVal) {
+        throw new Error(`Incorrect size value: '${this.size}'. ${rangeErrorMessage}`)
+      }
+      this.size = value;
+    },
+
+    addSelected(value: CellData) {
+      if (this.selected.includes(value)) return;
+      this.increaseTriesCount();
+      this.selected.push(value);
+    },
+
+    increaseTriesCount() {
+      if (this.step >= this.tries && this.result !== RESULTS.SUCCESS) {
+        this.result = RESULTS.FAIL
+      } else {
+        this.step++
+      }
+    }
+  }
+
+})
