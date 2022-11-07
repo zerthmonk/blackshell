1828693026982ea1a7257dba58c3240fa66c65f3
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-08 01:05:35 +0400

    Trace Cell component with ability to hint field cells

---
+++ app/src/components/hack/gridgame/TraceCell.vue
@@ -0,0 +1,41 @@
+<template>
+  <Cell class="trace-cell"
+        :hexValue="props.hexValue"
+        @mouseover="() => handleHover(true)"
+        @mouseout="() => handleHover(false)"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { useStore } from '@/stores/gridgame';
+import Cell from './Cell.vue';
+
+interface TraceCellProps {
+  hexValue: string | number;
+}
+
+const props = defineProps<TraceCellProps>();
+const store = useStore();
+
+function handleHover(value: boolean) {
+  console.log(props.hexValue, value)
+  store.setHighlighted(props.hexValue, value);
+}
+
+</script>
+
+<style scoped lang="scss">
+.trace-cell {
+  padding: 1rem;
+
+  &:hover {
+    border: 1px solid white;
+  }
+}
+
+.disabled {
+  pointer-events: none;
+  filter: brightness(50%);
+}
+</style>
