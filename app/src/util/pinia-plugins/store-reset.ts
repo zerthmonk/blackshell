f3c3282aa159ac728a7a482621ef199bcc41288c
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-10 16:11:37 +0400

    pinia reset plugin, lodash added to deps

---
+++ app/src/util/pinia-plugins/store-reset.ts
@@ -0,0 +1,11 @@
+import cloneDeep from 'lodash.clonedeep';
+import { StoreDefinition } from 'pinia';
+
+interface resetType {
+  store: StoreDefinition
+}
+
+export default function resetStore({ store }: resetType) {
+  const initialState = cloneDeep(store.$state)
+  store.$reset = () => store.$patch(cloneDeep(initialState))
+}
