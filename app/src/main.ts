55e01d929b9b9d926296b2c813a8b7682442cff9
Dmitry Bis <26793601+zerthmonk@users.noreply.github.com>
2022-11-10 19:13:40 +0400

    Pinia store plugin for resetting state (#8)
    * pinia reset plugin, lodash added to deps
    
    * lodash.deepcopy in deps only, we don't need whole lodash

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
+import piniaReset from './util/pinia-plugins/store-reset'
 import App from './App.vue'
 
+const pinia = createPinia()
+pinia.use(piniaPersist)
+pinia.use(piniaReset)
+
 createApp(App)
+    .use(pinia)
     .mount("#app")
