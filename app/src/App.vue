1543f8782f6f66fb367d103e1ab40c9a1f4f2bdd
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-19 21:08:38 +0400

    cleanup dirty merge

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,7 @@
 <template>
-<<<<<<< Updated upstream
 </template>
 
 <script setup lang="ts">
-=======
-  <Timer :seconds="10" :notifyOn="5"></Timer>
-</template>
-
-<script setup lang="ts">
-import Timer from "@/components/misc/CountdownTimer.vue";
->>>>>>> Stashed changes
 </script>
 
 <style lang="scss"></style>
