7854903b47076f77e72784f4539147bbf6b7329f
Dmitry Bis <26793601+zerthmonk@users.noreply.github.com>
2022-11-19 21:06:07 +0400

    ESLint + Pretter, formatted code (#9)
    * improved linter
    
    * ESLint + Prettier added, formatted code

--- app/src/components/base/TextBar.vue
+++ app/src/components/base/TextBar.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="textbar">
-    <div class="textbar__main"><span :class="textClass">{{message}}</span></div>
+    <div class="textbar__main">
+      <span :class="textClass">{{ message }}</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
   props: {
     message: String,
     textClass: {
       type: String,
-      default: "hdr-text"
-    }
-  }
-})
+      default: "hdr-text",
+    },
+  },
+});
 </script>
